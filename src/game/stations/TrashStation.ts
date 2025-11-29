60a52b79da0af811666671db302a9c1fdb587cee
Maid <zhp92@outlook.com>
2025-11-29 21:42:35 +0800

    feat: 添加垃圾桶交互动画

--- src/game/stations/TrashStation.ts
+++ src/game/stations/TrashStation.ts
@@ -1,18 +1,9 @@
 import { Station } from './Station';
 import { Player } from '../objects/Player';
-import { Item } from '../objects/Item';
 
 export class TrashStation extends Station {
   constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene, x, y, 'station_trash'); // 垃圾桶工作站
-  }
-
-  interact(player: Player) {
-    if (player.heldItem) {
-      // 销毁物品
-      player.heldItem.destroy();
-      player.heldItem = null;
-    }
   }
 
 
