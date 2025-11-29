100fe53afdde2426d86048a28f0ee76c3a9951ad
Maid <zhp92@outlook.com>
2025-11-29 12:30:19 +0800

    feat: 添加垃圾桶工作站动画

--- src/game/stations/TrashStation.ts
+++ src/game/stations/TrashStation.ts
@@ -15,7 +15,36 @@ export class TrashStation extends Station {
     }
   }
 
-  placeItem(item: Item): void {
-    item.destroy();
+
+  updateWhenIdle(delta: number): void {
+    if (!this.item) return;
+    this.workStatus = 'working';
   }
+
+  updateWhenWorking(delta: number): void {
+    if (!this.item) return;
+    this.item.rotation += delta * 0.01;
+    this.item.scale = Math.max(this.item.scale - delta * 0.001, 0);
+    console.log(this.item.scale)
+    this.scene.time.delayedCall(1000, () => {
+      this.item?.destroy();
+      this.item = null;
+      this.workStatus = 'done';
+    })
+  }
+
+  updateWhenDone() {
+    this.workStatus = 'idle';
+  }
+
+  // placeItem(item: Item): void {
+  //   if (this.item) return;
+  //   this.item = item;
+  //   this.item.rotation = this.scene.game.getTime();
+  //   this.item.scale = 1;
+  //   this.scene.time.delayedCall(500, () => {
+  //     this.item?.destroy();
+  //     this.item = null;
+  //   })
+  // }
 }
