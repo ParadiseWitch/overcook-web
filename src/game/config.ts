c907e91382631122b001be633b3142ee8e2e7b61
Maid <zhp92@outlook.com>
2025-11-29 22:01:25 +0800

    feat: 更新关卡地图，修复布局和元素位置

--- src/game/config.ts
+++ src/game/config.ts
@@ -6,17 +6,17 @@ export const SPEED_WALK = 180;
 export const SPEED_DASH = 750;
 export const DASH_TIME = 250;
 export const LEVEL_MAP = [
-  "XXTXXXXXXXXDXXTXX",
+  "XXTXXXXXDXXXXTXXX",
   "X               X",
-  "X . ..S   S..   X",
-  "X . .       .   X",
-  "X .             X",
+  "X   ..S   S..   X",
+  "X               X",
+  "X               X",
   "X               X",
   "B   1 a...a 2 KdX",
   "X               X",
   "X C           C X",
-  "X .           . X",
-  "X . ..B   B.. . X",
+  "X B           B X",
+  "X   C.B   B.C   X",
   "X               X",
   "XXXXXXXXXXXXXXXXX"
 ];
