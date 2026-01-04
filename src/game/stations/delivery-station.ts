041ee47053bc862a1b8b70526b243d863f82576e
Maid <zhp92@outlook.com>
2026-01-04 22:09:40 +0800

    refactor: 更新上菜点逻辑

--- src/game/stations/delivery-station.ts
+++ src/game/stations/delivery-station.ts
@@ -1,46 +1,58 @@
 import { DEPTH } from '../config';
+import { Item } from '../item';
 import { Plate } from '../item/container/plate';
 import { Station } from './station';
 
 export class DeliveryStation extends Station {
   score: number = 0;
   constructor(scene: Phaser.Scene, x: number, y: number) {
-    super(scene, x, y, 'station_delivery'); // 出餐口工作站
+    super(scene, x, y, 'station_delivery', false); // 出餐口工作站不允许放置物体
   }
 
-  update(delta: number): void {
-    super.update(delta);
-    if (!this.item || !(this.item instanceof Plate) || this.item.ingredients.length <= 0) return;
-    this.deliver();
+  public placeItem(item: Item): void {
+    if (!item) return;
+    if (!(item instanceof Plate)) return;
+    if (item.isEmpty()) return;
+    this.deliver(item);
+    super.placeItem(item);
   }
 
-  deliver() {
-    if (!this.item || !(this.item instanceof Plate) || this.item.ingredients.length <= 0) return;
-    if (this.item.ingredients[0].lastCookState() != 'boil') return;
-    this.item.clearIngredients();
-    this.item.destroy(); // 销毁汤物品
-    this.item = null;
+  deliver(plate: Plate) {
+    // 提示文字
+    let tipText = '';
+    // TODO:检测是否上菜成功
+    if (plate.ingredients[0].lastCookState() != 'boil') {
+      // 上菜失败
+      tipText = "上菜失败!";
+    } else {
+      tipText = "+100";
 
-    this.score += 100; // 增加得分
-    const scoreText = this.scene.children.getByName('scoreText') as Phaser.GameObjects.Text;
-    if (scoreText) {
-      scoreText.setText('得分: ' + this.score); // 更新得分显示
+      // 上菜成功
+      plate.clearIngredients();
+      plate.destroy(); // 销毁汤物品
+      this.item = null;
+      this.score += 100; // 增加得分
+
+      const scoreText = this.scene.children.getByName('scoreText') as Phaser.GameObjects.Text;
+      if (scoreText) {
+        scoreText.setText('得分: ' + this.score); // 更新得分显示
+      }
+
+      // 延迟3秒后在出餐口生成脏盘子
+      this.scene.time.delayedCall(3000, () => {
+        this.scene.events.emit('add-dirty-plate');
+      });
     }
-
-    // 显示得分提示文本
-    const deliveryText = this.scene.add.text(this.x, this.y - 30, '+100', { fontSize: '20px', color: '#ffff00' })
+    // 显示提示文本
+    const tipTextUi = this.scene.add.text(this.x, this.y - 30, tipText, { fontSize: '20px', color: '#ffff00' })
       .setOrigin(0.5).setDepth(DEPTH.UI);
     this.scene.tweens.add({
-      targets: deliveryText,
+      targets: tipTextUi,
       y: this.y - 60,
       alpha: 0,
       duration: 1000,
-      onComplete: () => deliveryText.destroy() // 动画结束后销毁文本
+      onComplete: () => tipTextUi.destroy() // 动画结束后销毁文本
     });
 
-    // 延迟3秒后在出餐口生成脏盘子
-    this.scene.time.delayedCall(3000, () => {
-      this.scene.events.emit('add-dirty-plate');
-    });
   }
 }
