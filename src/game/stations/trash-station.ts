2e3cdb27c8a237833f95a9533b2e0f9f2e7e1e23
Maid <zhp92@outlook.com>
2026-01-05 23:01:38 +0800

    fix: 更新垃圾桶逻辑
    - 垃圾桶不可以放置物体（直接丢弃）
    - 对于容器，只能角色手持容器丢弃

--- src/game/stations/trash-station.ts
+++ src/game/stations/trash-station.ts
@@ -1,35 +1,54 @@
+import { DEPTH } from '../config';
+import { Item } from '../item';
+import { Container } from '../item/container/container';
+import { Ingredient } from '../item/ingredient/ingredient';
 import { Station } from './station';
 
 export class TrashStation extends Station {
   constructor(scene: Phaser.Scene, x: number, y: number) {
-    super(scene, x, y, 'station_trash'); // 垃圾桶工作站
+    super(scene, x, y, 'station_trash', false);
   }
 
-
-  updateWhenIdle(_delta: number): void {
-    if (!this.item) return;
-    this.workStatus = 'working';
+  public placeItem(item: Item): void {
+    if (!item) return;
+    this.trash(item);
   }
 
-  updateWhenWorking(_delta: number): void {
-    if (!this.item) return;
-    this.scene.tweens.add({
-      targets: this.item,
-      rotation: "+=10",
-      scale: "0",
-      // alpha: 0,
-      duration: 1000,
-      onComplete: () => {
-        // TODO:如果角色拿着容器交互,应该是将容器中的食材倒入垃圾桶
-        this.item?.destroy();
-        this.item = null;
-        this.workStatus = 'done';
+  trash(item: Item) {
+    if (item instanceof Container && !item.isFlying) {
+      item.ingredients.forEach(i => i.destroy());
+      item.ingredients = [];
+      // HACK: 
+      // 需求：容器只有角色手动交互时才可以倒掉食材，无法通过throw的方式倒掉食材
+      // 倒掉之后要将空容器拿在手上
+      const player = item.heldBy;
+      this.scene.time.delayedCall(100, () => {
+        if (player) {
+          player.pick(item);
+        }
+      });
+      return;
+    }
+
+    if (item instanceof Ingredient) {
+      item.isFlying = false; // 物品不再飞行
+      item.heldBy = null; // 物品不再被持有
+      if (item.body) {
+        item.body.enable = false; // 禁用物理碰撞
+        item.setVelocity(0, 0); // 停止移动
       }
-    });
-  }
+      item.x = this.x;
+      item.y = this.y;
+      item.setDepth(DEPTH.ITEM); // 设置物品层级
+      this.scene.tweens.add({
+        targets: item,
+        rotation: "+=10",
+        scale: "0",
+        duration: 1000,
+        onComplete: () => item.destroy()
+      });
+    }
 
-  updateWhenDone() {
-    this.workStatus = 'idle';
   }
 
 }
